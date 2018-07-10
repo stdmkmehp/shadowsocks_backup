25ec37a7ff149c4c0eaebdfbabd0fd1246c6f4f1
stdmkmehp <zhaowasd@foxmail.com>
2018-07-10 14:09:57 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+apt-get install python-pip python-setuptools && pip install git+https://github.com/shadowsocks/shadowsocks.git@master && cp ./*.json /etc/ && ssserver -c /etc/shadowsocks_ipv6.json -d start
