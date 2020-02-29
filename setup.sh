4ef3a2e3ae78edf4bd0c46092fc7c5ae6e8246fe
dotdotdot <xl2013919@sina.cn>
2020-02-29 19:16:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update && 
-apt-get install python-pip python-setuptools && 
+apt-get install -y python-pip python-setuptools && 
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master && 
 cp ./*.json /etc/ && 
 ssserver -c /etc/shadowsocks_ipv6.json -d start
